42d28b360e18bac473c55e8b8228e739d3ed8498
Tanmay Rajgor <rajgortanmay95@gmail.com>
2021-02-18 13:31:20 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int score1 = 50;
+    int score2 = 60;
+    int score3 = 70;
+    
+    printf("Average: %f\n", (score1 + score2 + score3) / 3.0);
+}
